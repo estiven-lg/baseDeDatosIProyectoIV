9032a8fd166756b487ecddbe8e8de96b5cd79271
estiven-lg <estivenlaferre@gmail.com>
2024-11-01 21:09:40 -0600

    ...

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -5,7 +5,7 @@ const oracledb = require('oracledb');
 module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    connectionString: "192.168.0.20:1521/XE",
+    connectionString: process.env.DB_CONNECTION_STRING,
     poolMin: 5,
     poolMax: 10,
     poolIncrement: 1,
